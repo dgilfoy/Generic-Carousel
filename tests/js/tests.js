7a01e61d2b2c3226028e340add1cb2ccfc98f7b5
Daniel Gilfoy <dgilfoy@gmail.com>
2014-06-12 08:06:31 -0700

    changes to tests - use assert rather than funciton aliases

--- tests/js/tests.js
+++ tests/js/tests.js
@@ -21,9 +21,9 @@
     var tests = new TestSuite()
       .then( function(slider) {
         return deferTest( function(){
-            test('New Carousel', 2, function( ) {
-                ok( typeof slider === 'object', "New Object Created" );
-                ok( !slider.hasOwnProperty('config'), 'Object is not initalized');
+            test('New Carousel', 2, function ( assert ) {
+                assert.equal( typeof slider, 'object', "New Object Created");
+                assert.ok( !slider.hasOwnProperty('config'), 'Object is not initalized');
                 return slider;
             });        
         }, slider);
@@ -32,14 +32,14 @@
         var opts = {
             wrapperClass: 'carousel-wrapper',
             thumbnails: true,
-            delay: 1000  
+            delay: 1000 
         };
         slider.init( opts );
         return deferTest( function () {
-            test('Initialize Carousel Settings', 3, function() {
-                ok( slider.hasOwnProperty('config'), 'Object is initalized');
-                ok( slider.config.wrapperClass === opts.wrapperClass, 'Wrapper class is successfully overwritten');
-                ok( slider.slides[slider.current].className === 'slide active', 'Initial slide is set to active');
+            test('Initialize Carousel Settings', 3, function(assert) {
+                assert.ok( slider.hasOwnProperty('config'), 'Object is initalized');
+                assert.ok( slider.config.wrapperClass === opts.wrapperClass, 'Wrapper class is successfully overwritten');
+                assert.ok( slider.slides[slider.current].className === 'slide active', 'Initial slide is set to active');
             });    
         }, slider );
         // see if Carousel has been initialized and wrapper and initial slides are set to visible (class="active")
@@ -47,60 +47,60 @@
     })
       .then( function (slider) {
         return deferTest( function () {
-            test('Carousel Next', 1, function() {
+            test('Carousel Next', 1, function(assert) {
                 slider.next();
-                ok(slider.current > 0, 'Slide element is incremented');
+                assert.ok(slider.current > 0, 'Slide element is incremented');
             });   
         }, slider );
     })
       .then( function (slider) {
         return deferTest( function () {
-            test('Carousel Previous', 1, function() {
+            test('Carousel Previous', 1, function(assert) {
                 slider.prev();
-                ok(slider.current === 0, 'Slide element is decremented');
+                assert.ok(slider.current === 0, 'Slide element is decremented');
             });   
         }, slider, 2000 );
     })
       .then( function (slider) {
         return deferTest( function () {
-            test('Enable Slide In Animation', 2, function() {
+            test('Enable Slide In Animation', 2, function(assert) {
                 slider.addConfig({
                     "animationIn" : "slidein",
                     "animationOut": "slideout"
                 });
-                ok(slider.config.animationIn, 'Slide In animation is added');
-                ok(slider.config.animationOut, 'Slide Out animation is added');
+                assert.ok(slider.config.animationIn, 'Slide In animation is added');
+                assert.ok(slider.config.animationOut, 'Slide Out animation is added');
             });   
         }, slider );
     })
       .then( function (slider) {
         return deferTest( function () {
-            test('Carousel AutoPlay', 1, function() {
+            test('Carousel AutoPlay', 1, function(assert) {
                 slider.play();
-                ok(slider.interval, 'Slide element is playing');
+                assert.ok(slider.interval, 'Slide element is playing');
             });   
         }, slider );
     })
       .then( function (slider) {
         return deferTest( function () {
-            test('Carousel AutoPlay Off', 1, function() {
+            test('Carousel AutoPlay Off', 1, function(assert) {
                 slider.stopPlay();
-                ok( !slider.interval, 'Slide element is stopped');   
+                assert.ok( !slider.interval, 'Slide element is stopped');   
             } );
         }, slider, 4000);
     })
       .then( function (slider) {
         return deferTest( function () {
-            test('Add thumbnails', 1, function() {
+            test('Add thumbnails', 1, function(assert) {
                 slider.addConfig({"thumbs" : { "show" : true}});
                 slider.addThumbnails();
-                ok( slider.config.thumbs.show, 'Add Thumbnails');
+                assert.ok( slider.config.thumbs.show, 'Add Thumbnails');
                 // maybe add test to see if thumbnails were added to the DOM  
             } );
-        }, slider, 5000);
+        }, slider, 3000);
     })
       .then( function (slider) {
-        return deferTest( function () {
+        return deferTest( function (assert) {
             test('Click to first slide', 1, function() {
                slider.selectSlide(0);
                ok(slider.current === 0, 'Slider is set to first slide');
